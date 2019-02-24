fb68c496e2d9382ecfd2a2ee0870a9b6b1710d8c
Aleksandr <aleksandrkopchikov@ya.ru>
2019-02-24 22:48:22 +0300

    Выполнил ДЗ второго занятия.

---
+++ MyClassLibrary/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Helper
+    {
+
+        public static string IsOdd(int n)
+        {
+            if (n % 2 == 0) return ("Да");
+            else return ("Нет");
+        }
+
+        static public void Print(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+        }
+
+    }
+}
